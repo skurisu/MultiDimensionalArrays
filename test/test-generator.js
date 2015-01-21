c992029eed5fbbcffcdf38bf240d946f8794121f
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-21 09:07:59 -1000

    adds some test information and module export

---
+++ test/test-generator.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var mda = require('../MultiDimensional_Arrays.js');
+ 
+ 
+describe('generate function will return 3 arrays with a length of 3', function() {
+  it('should do something', function() {
+    var something = "some value";
+    expect(something).to.be.a('string');
+    something.should.equal('some value');
+  });
+});
