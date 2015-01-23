120ee2b5c1eea8ab526320c86a46d22e387f35ae
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-22 23:18:39 -1000

    adds passing tests for generate_3d

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,19 +1,58 @@
 module.exports = {
-  generate_1d : function() {
-    // var test = [];
-    // for(var i = 0; i < 3; i++){
-    //   test.push(Math.round(Math.random()));
-    // }
-    // console.log(test);
-    // return test;
+  generate_1d : function(param) {
+    var result = [];
+
+    if(param === undefined){
+      param = 3;
+    } 
+    if (typeof param !== 'number') {
+      return result;
+    }
+    // code logic here 
+    while(result.length < param){
+      result.push(Math.round(Math.random()));
+    }
+    return result;
   },
 
-  generate_2d : function ( ) {
-    // body...
+  generate_2d : function (param1,param2) {
+    var result = [];
+
+    //if param1 and param2 are undefined 
+    if(param1 === undefined) { param1 = 3; }
+    if(param2 === undefined) { param2 = 3; }
+
+    if(typeof param1 !== 'number' || typeof param2 !== 'number') {
+      return result;
+    }
+    //code logic here
+    //determine array length
+    while(result.length < param1) {
+      result.push(this.generate_1d(param2));
+    }
+
+    return result; 
   },
 
-  generate_3d : function ( ) {
-    // body...
+  generate_3d : function (param1,param2,param3) {
+    var result = [];
+
+    if(param1 === undefined) { param1 = 3; }
+    if(param2 === undefined) { param2 = 3; }
+    if(param3 === undefined) { param3 = 3; }
+
+    if(typeof param1 !== 'number' || typeof param2 !== 'number' || typeof param3 !== 'number') {
+      return result;
+    }
+    //code logic here
+    //determine array length
+    while(result.length < param1) {
+      result.push(this.generate_2d(param2,param3));
+    }
+    
+    return result;
   }
 
+
 };
+
--- test/test-generator.js
+++ test/test-generator.js
@@ -9,6 +9,7 @@ describe('generate_1d ', function() {
     //result = [0,1,1];
     var testResult = mda.generate_1d();
 
+    expect(testResult).to.be.a("array");
     expect(testResult).to.have.length(3);
   });
 
@@ -20,7 +21,6 @@ describe('generate_1d ', function() {
         return val === 0 || val === 1;
       });
     }
-
   });
 
   it('should return an array with four boolean values when a single argument is given with a value of 4', function() {
@@ -39,20 +39,22 @@ describe('generate_1d ', function() {
 
     expect(testResult).to.be.a("array");
     expect(testResult).to.have.length(0);
+    });
   });
-});
 
 describe('generate_2d', function() {
-  it('should return an array of length 3, each containing arrays with 3 random boolean values', function(){     
+  it('should return an array of length 3, each containing arrays with 3 random boolean values with no argument', function(){     
     var testResult = mda.generate_2d();
 
     expect(testResult).to.be.a("array");
     expect(testResult).to.have.length(3);
     for(var i = 0; i < testResult.length; i++){
       expect(testResult[i]).to.have.length(3);
-      expect(testResult[i]).to.satisfy(function (val) {
+      for(var j = 0; j < testResult[i].length; j++){
+        expect(testResult[i][j]).to.satisfy(function (val) {
         return val === 0 || val === 1;
-      });
+        });
+      }
     }
   });
 
@@ -60,12 +62,14 @@ describe('generate_2d', function() {
     var testResult = mda.generate_2d(4);
 
     expect(testResult).to.be.a("array");
-    expect(testResult.to.have.length(4));
+    expect(testResult).to.have.length(4);
     for(var i = 0; i < testResult.length; i++){
       expect(testResult[i]).to.have.length(3);
-      expect(testResult[i]).to.satisfy(function (val) {
+      for(var j = 0; j < testResult[i].length; j++){
+      expect(testResult[i][j]).to.satisfy(function (val) {
         return val === 0 || val === 1;
-      });
+        });
+      } 
     }
   });
 
@@ -76,14 +80,31 @@ describe('generate_2d', function() {
     expect(testResult).to.have.length(5);
     for(var i = 0; i < testResult.length; i++){
       expect(testResult[i]).to.have.length(4);
-      expect(testResult[i]).to.satisfy(function (val) {
-        return val === 0 || val === 1;
-      });
+      for(var j = 0; j < testResult[i].length; j++){
+        expect(testResult[i][j]).to.satisfy(function (val) {
+          return val === 0 || val === 1;
+        });
+      }
+    }
+  });  
+});
+
+describe("generate_3d", function() {
+  it("should take three arguments (3,4,5), return an array (length 3) containing arrays (length 4) with 5 boolean values each", function() {
+    var testResult = mda.generate_3d(3,4,5);
+
+    expect(testResult).to.be.a("array");
+    expect(testResult).to.have.length(3);
+    for(var i = 0; i < testResult.length; i++){
+      expect(testResult[i]).to.have.length(4);
+      for(var j = 0; j < testResult[i].length; j++){
+        expect(testResult[i][j]).to.have.length(5);
+        for(var k = 0; k < testResult[i][j].length; k++){
+          expect(testResult[i][j][k]).to.satisfy(function (val) {
+            return val === 0 || val === 1;
+          });
+        }
+      }
     }
   });
-
-  it("should ", function() {
-    
-  });
-  
 });
